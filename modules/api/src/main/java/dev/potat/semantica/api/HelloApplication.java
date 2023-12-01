65e5410cc32953785da20e1db347bbb0402f1ac2
Den <denis100703@gmail.com>
2023-12-01 05:30:08 +0300

    API template created

---
+++ modules/api/src/main/java/dev/potat/semantica/api/HelloApplication.java
@@ -0,0 +1,9 @@
+package dev.potat.semantica.api;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
